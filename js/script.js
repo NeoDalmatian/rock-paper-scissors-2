4e8b0117a371b0e62441589513292920886b579b
NeoDalmatian <aviskovic100@gmail.com>
2023-05-02 16:04:38 +0200

    Add function game and function reset
    -Add three new variables that track result of rounds and how many of
    them were played.
    -Modify round() function so it puts input to new variables.
    -Add function game() that plays round() 5 times and it outputs result of
    those games.
    	-For loop uses numberOfRounds for its syntax because if player
    	enters wrong input it shouldn't count towards total number of
    	games played, round() invalid input changes numberOfRounds by -1
    	-When number of rounds reaches 5, it triggers reset().
    -Add function reset() for reseting variables for tracking results and
    number of rounds.
    -FIX function getComputerChoice() - randomNumber(3) get saved in
    variable and then that variable gets converted to string.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
+let playerResult = 0;
+let computerResult = 0;
+let numberOfRounds = 0;
+
 function randomNumber (n) {
   return Math.floor(Math.random() * n) + 1;
 }
 
 function getComputerChoice () {
-  return randomNumber(3) === 1? "ROCK": randomNumber(3) === 2? "PAPER": "SCISSORS";
+  let conversion = randomNumber(3);
+  return conversion === 1? "ROCK": conversion === 2? "PAPER": "SCISSORS";
 }
 
 function round (playerSelection, computerSelection = getComputerChoice()) {
@@ -13,12 +18,40 @@ function round (playerSelection, computerSelection = getComputerChoice()) {
   } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS" ||
       playerSelection === "PAPER" && computerSelection === "ROCK" ||
       playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+    playerResult++;
     return `You WON this round! ${playerSelection} beats ${computerSelection}!`;
   } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK" ||
       playerSelection === "ROCK" && computerSelection === "PAPER" ||
       playerSelection === "PAPER" && computerSelection === "SCISSORS"){
+    computerResult++;
     return `You LOST this round! ${playerSelection} loses to ${computerSelection}!`;
   } else {
-    return `${playerSelection} in invalid input, please enter either rock, paper or scissors`
+    numberOfRounds--;
+    return `${playerSelection} is invalid input, either enter rock, paper or scissors`
   }
+}
+ 
+function game() {
+  for (numberOfRounds = 0; numberOfRounds < 5; numberOfRounds++) {
+    console.log(round(prompt()));
+  }
+
+  if (numberOfRounds === 5) {
+    if (playerResult === computerResult) {
+      console.log("It's a tie!")
+      reset();
+    } else if (playerResult > computerResult) {
+      console.log("You WON the game!")
+      reset();
+    } else if (playerResult < computerResult) {
+      console.log("You LOST the game!")
+      reset();
+    }
+  }
+}
+
+function reset() {
+  playerResult = 0;
+  computerResult = 0;
+  numberOfRounds = 0;
 }
