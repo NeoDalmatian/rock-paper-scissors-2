f97a401efc820fd0e7d3896160f46c65e67f4cfa
NeoDalmatian <aviskovic100@gmail.com>
2023-05-02 14:46:04 +0200

    Add function that plays one round
    -Add function round() that uses player choice and computer choice (with
    function getComputerChoice()), compares results of its arguments and
    acording to the rules od the game, it outputs either win, loose or tie
    massage.
    -Player input is case insensitive.
    -If player input isn't either rock, paper or scissors it outputs error
    massage.

--- js/script.js
+++ js/script.js
@@ -5,3 +5,20 @@ function randomNumber (n) {
 function getComputerChoice () {
   return randomNumber(3) === 1? "ROCK": randomNumber(3) === 2? "PAPER": "SCISSORS";
 }
+
+function round (playerSelection, computerSelection = getComputerChoice()) {
+  playerSelection = playerSelection.toUpperCase();
+  if (playerSelection === computerSelection) {
+    return `This round is TIE! ${playerSelection} and ${computerSelection} are equal.`;
+  } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS" ||
+      playerSelection === "PAPER" && computerSelection === "ROCK" ||
+      playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+    return `You WON this round! ${playerSelection} beats ${computerSelection}!`;
+  } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK" ||
+      playerSelection === "ROCK" && computerSelection === "PAPER" ||
+      playerSelection === "PAPER" && computerSelection === "SCISSORS"){
+    return `You LOST this round! ${playerSelection} loses to ${computerSelection}!`;
+  } else {
+    return `${playerSelection} in invalid input, please enter either rock, paper or scissors`
+  }
+}
