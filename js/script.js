aceb540b1e1e5693d2e939f19c9cf58a1067f1bd
NeoDalmatian <aviskovic100@gmail.com>
2023-05-17 20:55:31 +0200

    Add final result after 5 games, add reset
    -Add function result() that tracks number of rounds and compares player
    result with computer result and determines a winner, it displays result
    of a 5 game mach on a display. After 5 rounds it fires reset().
    -Add function reset() that resets number of rounds and tracker for
    computer and player result.
    -Select reset button, add click event listener that fires reset() and
    displays default message on display.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let playerButtons = document.getElementById("buttons").childNodes;
 let displayText = document.querySelector("h1");
-
+let resetButton = document.querySelector(".reset");
+// add UI for counters
 let playerResult = 0;
 let computerResult = 0;
 let numberOfRounds = 0;
@@ -11,6 +12,11 @@ playerButtons.forEach(button => {
   });
 });
 
+resetButton.addEventListener("click", () => {
+  displayText.textContent = "Play the game!";
+  reset();
+});
+
 function randomNumber (n) {
   return Math.floor(Math.random() * n) + 1;
 }
@@ -19,39 +25,43 @@ function getComputerChoice () {
   let conversion = randomNumber(3);
   return conversion === 1? "ROCK": conversion === 2? "PAPER": "SCISSORS";
 }
-
+//swap condition of else if to if so it looks neater
 function round (playerSelection, computerSelection = getComputerChoice()) {
   if (playerSelection === computerSelection) {
     displayText.textContent =
      `This round is a tie, you both chose ${playerSelection}`;
+    numberOfRounds++;
   } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS" ||
              playerSelection === "PAPER" && computerSelection === "ROCK" ||
              playerSelection === "SCISSORS" && computerSelection === "PAPER") {
     displayText.textContent =
      `You WON this round! ${playerSelection} beats ${computerSelection}!`;
+     numberOfRounds++;
+     playerResult++;
   } else {
     displayText.textContent =
      `You LOST this round! ${playerSelection} loses to ${computerSelection}!`;
+     numberOfRounds++;
+     computerResult
   }
+  result();
 }
- 
-// function game() {
-//   playerResult = 0;
-//   computerResult = 0;
-//   numberOfRounds = 0;
 
-//   while (numberOfRounds < 5) {
-//     console.log(round(prompt("Please enter: rock, paper or scissors.")));
-//     numberOfRounds++;
-//   }
+function result () {
+  if (numberOfRounds === 5) {
+    if (playerResult === computerResult) {
+      displayText.textContent = "It's a tie!"
+    } else if (playerResult > computerResult) {
+      displayText.textContent = "You WON the game!"
+    } else if (playerResult < computerResult) {
+      displayText.textContent = "You LOST the game!"
+    }
+    reset();
+  } 
+}
 
-//   if (numberOfRounds === 5) {
-//     if (playerResult === computerResult) {
-//       console.log("It's a tie!")
-//     } else if (playerResult > computerResult) {
-//       console.log("You WON the game!")
-//     } else if (playerResult < computerResult) {
-//       console.log("You LOST the game!")
-//     }
-//   }
-// }
+function reset () {
+  numberOfRounds = 0;
+  playerResult = 0;
+  computerResult = 0;
+}
